5c00a41ffccc3e99e8230461e196de17b35cf02b
Aime Patrick <aimepatrick64@gmail.com>
2025-05-21 00:40:35 +0200

    unused

--- src/utils/toast.tsx
+++ src/utils/toast.tsx
@@ -1,4 +1,4 @@
-import React, { type ReactElement } from 'react';
+import { type ReactElement } from 'react';
 import { Toaster } from 'react-hot-toast';
 import type { ToastPosition } from 'react-hot-toast';
 import { FaInfoCircle } from 'react-icons/fa';
